8a5988332612a804f171236ad01899d19b019865
Александр Гренишин <nd0ut.me@gmail.com>
2018-04-26 20:10:18 +0300

    adapt code for redactor 3

--- uploadcare.js
+++ uploadcare.js
@@ -1,82 +1,85 @@
 /*
- * Uploadcare Redactor plugin (1.6.0)
+ * Uploadcare Redactor plugin (2.0.0)
  *
  */
 
-(function($) {
-    $.Redactor.prototype.uploadcare = function() {
-        var $opts;
-        return {
-            init: function() {
-                $opts = $.extend({}, this.opts.uploadcare);
-                // defaults
-                if (!$opts.crop) {
-                    $opts.crop = '';
-                }
-                if (!$opts.version) {
-                    $opts.version = '2.x';
-                }
+(function($, $R) {
+    $R.add('plugin', 'uploadcare', {
+        init: function(app) {
+            this.app = app;
+            this.toolbar = app.toolbar;
 
-                // Use imageTag from redactor config
-                if (this.opts.imageTag) {
-                    $opts.imageTag = this.opts.imageTag;
-                }
+            this.opts = $.extend({}, this.app.opts.uploadcare);
+            // defaults
+            if (!this.opts.crop) {
+                this.opts.crop = '';
+            }
+            if (!this.opts.version) {
+                this.opts.version = '3.x';
+            }
 
-                if (typeof uploadcare === 'undefined') {
-                    var widget_url = 'https://ucarecdn.com/libs/widget/' + $opts.version + '/uploadcare.min.js';
-                    $.getScript(widget_url);
-                }
-                
-                var button = $opts.buttonBefore
-                    ? this.button.addBefore($opts.buttonBefore, 'uploadcare', $opts.buttonLabel || 'Uploadcare')
-                    : this.button.add('uploadcare', $opts.buttonLabel || 'Uploadcare');
+            // Use imageTag from redactor config
+            if (this.app.opts.imageTag) {
+                this.opts.imageTag = this.opts.imageTag;
+            }
 
-                this.button.addCallback(button, this.uploadcare.show);
+            if (typeof uploadcare === 'undefined') {
+                var widget_url = 'https://ucarecdn.com/libs/widget/' + this.opts.version + '/uploadcare.min.js';
+                $.getScript(widget_url);
+            }
+        },
 
-                if ($opts.buttonIconEnabled) {
-                    this.button.setIcon(button, '<i class="' + ($opts.buttonIcon ? $opts.buttonIcon : 're-icon-file') + '"></i>');
-                }
-            },
+        start: function() {
+            var buttonData = { 
+                title: this.opts.buttonLabel || 'Uploadcare', 
+                api: 'plugin.uploadcare.show'
+            }
+            
+            var button = this.opts.buttonBefore
+                ? this.toolbar.addButtonBefore(this.opts.buttonBefore, 'uploadcare', buttonData)
+                : this.toolbar.addButton('uploadcare', buttonData);
 
-            show: function() {
-                var $this = this;
-                var dialog = uploadcare.openDialog({}, $opts);
-                this.core.callback('uploadcareShow', dialog, $opts);
-                this.selection.save();
-                dialog.fail(function() {
-                    $this.selection.restore();
-                    $this.core.callback('uploadcareCancel', Array.prototype.slice.call(arguments));
-                });
-                dialog.done(this.uploadcare.done);
-            },
+            if (this.opts.buttonIconEnabled) {
+                button.setIcon('<i class="' + (this.opts.buttonIcon ? this.opts.buttonIcon : 're-icon-file') + '"></i>');
+            }  
+        },
 
-            done: function(data) {
-                var $this = this;
-                var files = $opts.multiple ? data.files() : [data];
-                this.selection.restore();
-                $.when.apply(null, files).done(function() {
-                    var resolvedFiles = Array.prototype.slice.call(arguments);
-                    $.each(resolvedFiles, function() {
-                        if ($.isFunction($opts.uploadCompleteCallback)) {
-                            $opts.uploadCompleteCallback.call($this, this);
-                        } else {
-                            var imageUrl = this.cdnUrl;
-                            if (this.isImage && !this.cdnUrlModifiers) {
-                                imageUrl += '-/preview/';
-                            }
-                            if (this.isImage) {
-                                var openTag = $opts.imageTag ? '<' + $opts.imageTag + '>' : '';
-                                var closeTag = $opts.imageTag ? '</' + $opts.imageTag + '>' : '';
-                                $this.insert.html(openTag + '<img src="' + imageUrl + '" alt="' + this.name + '" data-image="' + this.uuid + '" />' + closeTag, false);
-                            } else {
-                                $this.insert.html('<a href="' + this.cdnUrl + '" data-file="' + this.uuid + '">' + this.name + '</a>', false);
-                            }
+        show: function() {
+            var $this = this;
+            var dialog = uploadcare.openDialog({}, this.opts);
+            $this.app.selection.save();
+            $this.app.broadcast('uploadcareShow', dialog, this.opts);
+            
+            dialog.fail(function() {
+                $this.app.selection.restore();
+                $this.app.broadcast('uploadcareCancel', Array.prototype.slice.call(arguments));
+            });
+            dialog.done(this.done.bind(this));
+        },
+
+        done: function(data) {
+            var $this = this;
+            var files = this.opts.multiple ? data.files() : [data];
+            this.app.selection.restore();
+            $.when.apply(null, files).done(function() {
+                var resolvedFiles = Array.prototype.slice.call(arguments);
+                $.each(resolvedFiles, function() {
+                    if ($.isFunction($this.opts.uploadCompleteCallback)) {
+                        $this.opts.uploadCompleteCallback.call($this, this);
+                    } else {
+                        var imageUrl = this.cdnUrl;
+                        if (this.isImage && !this.cdnUrlModifiers) {
+                            imageUrl += '-/preview/';
                         }
-                    });
-                    $this.storage.observe(); // Trigger storage tracking
-                    $this.core.callback('uploadcareDone', resolvedFiles);
+                        if (this.isImage) {
+                            $this.app.api('module.image.insert', { image: { url: imageUrl, alt: this.name, id: this.uuid } });
+                        } else {
+                            $this.app.api('module.link.insert', { url: this.cdnUrl, text: this.name, id: this.uuid });
+                        }
+                    }
                 });
-            },
-        };
-    };
-})(jQuery);
+                $this.app.broadcast('uploadcareDone', resolvedFiles);                
+            });
+        },
+    })
+})(jQuery, Redactor);
