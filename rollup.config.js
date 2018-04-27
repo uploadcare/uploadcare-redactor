e9e93a1705b07ddb996858402207276814434eef
Александр Гренишин <nd0ut.me@gmail.com>
2018-04-27 23:53:15 +0300

    generate minified versions

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,54 @@
-import jscc from 'rollup-plugin-jscc'
-import license from 'rollup-plugin-license'
-import pkg from './package.json'
+import jscc from "rollup-plugin-jscc";
+import license from "rollup-plugin-license";
+import pkg from "./package.json";
+import uglify from "rollup-plugin-uglify";
 
 const getPlugins = redactorVer => [
   jscc({
     values: {
       _REDACTOR_TARGET: redactorVer,
-      _WIDGET_VERSION: pkg.widgetVersion,
-    },
+      _WIDGET_VERSION: pkg.widgetVersion
+    }
   }),
   license({
     banner: `
       Uploadcare Redactor ${redactorVer} plugin
       Version: <%= pkg.version %>
-    `,
-  }),
-]
+    `
+  })
+];
 
 export default [
   {
-    input: 'src/uploadcare.js',
+    input: "src/uploadcare.js",
     plugins: getPlugins(3),
     output: {
-      file: 'dist/uploadcare.redactor3.js',
-      format: 'iife',
-    },
+      file: "dist/uploadcare.redactor3.js",
+      format: "iife"
+    }
   },
   {
-    input: 'src/uploadcare.js',
+    input: "src/uploadcare.js",
+    plugins: [uglify(), ...getPlugins(3)],
+    output: {
+      file: "dist/uploadcare.redactor3.min.js",
+      format: "iife"
+    }
+  },
+  {
+    input: "src/uploadcare.js",
     plugins: getPlugins(2),
     output: {
-      file: 'dist/uploadcare.redactor2.js',
-      format: 'iife',
-    },
+      file: "dist/uploadcare.redactor2.js",
+      format: "iife"
+    }
   },
-]
+  {
+    input: "src/uploadcare.js",
+    plugins: [uglify(), ...getPlugins(2)],
+    output: {
+      file: "dist/uploadcare.redactor2.min.js",
+      format: "iife"
+    }
+  }
+];
