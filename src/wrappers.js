1a6690893e1d7fae06fff88f1d8a56adda9d210c
Александр Гренишин <nd0ut.me@gmail.com>
2018-04-29 10:49:38 +0300

    remove 'getRedactorVersion' func

--- src/wrappers.js
+++ src/wrappers.js
@@ -1,8 +1,6 @@
-import getRedactorVersion from './getRedactorVersion'
 import * as redactor3 from './redactor3/index'
 import * as redactor2 from './redactor2/index'
 
-var redactorVersion = getRedactorVersion()
-var wrappers = redactorVersion === 3 ? redactor3 : redactor2
+var wrappers = redactor3.getVersion() ? redactor3 : redactor2
 
 export default wrappers
