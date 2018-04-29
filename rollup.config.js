8de71a2a4efd8409b539b85b81ae309f3b0066c6
Александр Гренишин <nd0ut.me@gmail.com>
2018-04-29 10:55:02 +0300

    lint not only src

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,33 @@
-import jscc from "rollup-plugin-jscc";
-import license from "rollup-plugin-license";
-import pkg from "./package.json";
-import uglify from "rollup-plugin-uglify";
+import jscc from 'rollup-plugin-jscc'
+import license from 'rollup-plugin-license'
+import pkg from './package.json'
+import uglify from 'rollup-plugin-uglify'
 
 const plugins = [
-  jscc({
-    values: {
-      _WIDGET_VERSION: pkg.widgetVersion
-    }
-  }),
+  jscc({values: {_WIDGET_VERSION: pkg.widgetVersion}}),
   license({
     banner: `
       Uploadcare Redactor 2/3 plugin
       Version: <%= pkg.version %>
-    `
-  })
-];
+    `,
+  }),
+]
 
 export default [
   {
-    input: "src/uploadcare.js",
+    input: 'src/uploadcare.js',
     plugins: plugins,
     output: {
-      file: "dist/uploadcare.redactor.js",
-      format: "iife"
-    }
+      file: 'dist/uploadcare.redactor.js',
+      format: 'iife',
+    },
   },
   {
-    input: "src/uploadcare.js",
+    input: 'src/uploadcare.js',
     plugins: [uglify(), ...plugins],
     output: {
-      file: "dist/uploadcare.redactor.min.js",
-      format: "iife"
-    }
-  }
-];
+      file: 'dist/uploadcare.redactor.min.js',
+      format: 'iife',
+    },
+  },
+]
